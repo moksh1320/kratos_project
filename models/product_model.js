37f05b186f99c8589dbdab461b788392ae6bc017
moksh1320 <moksh111320@gmail.com>
2020-02-08 13:23:17 +0530

    node commit by moksh 8/2

--- models/product_model.js
+++ models/product_model.js
@@ -10,7 +10,7 @@ var product={
         return db.query('select * from product_tbl where p_id=?',[p_id],callback);
     },
     updateProduct:function(item,callback){
-        return db.query('update product_tbl set p_name=?,p_price=?,p_qty=?,p_stock=?,p_dis=?,p_unit=?,p_usage=?,fk_sct_id=? where p_id=?',[item.p_name,item.p_price,item.p_qty,item.p_stock,item.p_dis,item.p_unit,item.p_usage,item.fk_sct_id,item.p_id],callback);
+        return db.query('update product_tbl set p_name=?,p_price=?,p_qty=?,p_stock=?,p_dis=?,p_unit=?,p_usage=?,p_disc=?,fk_sct_id=? where p_id=?',[item.p_name,item.p_price,item.p_qty,item.p_stock,item.p_dis,item.p_unit,item.p_usage,item.p_disc,item.fk_sct_id,item.p_id],callback);
     },
     deleteProduct:function(p_id,callback){
         return db.query('delete from product_tbl where p_id=?',[p_id],callback);
