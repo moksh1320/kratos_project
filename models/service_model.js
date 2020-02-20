075d6fe426edf94f4d87ef55451907ad3a2b159b
moksh1320 <moksh111320@gmail.com>
2020-02-20 16:07:35 +0530

    commit node from 20/2

---
+++ models/service_model.js
@@ -0,0 +1,19 @@
+var db=require('../dbconnection');
+var Service={
+    getAllservice:function(callback){
+        return db.query('select * from service_tbl',callback);
+    },
+    getServiceById:function(s_id,callback){
+        return db.query('select * from service_tbl where s_id=?',[s_id],callback);
+    },
+    updateService:function(item,callback){
+        return db.query('update service_tbl set s_name=?,s_price=?,s_dur,s_disc=?,s_ben where s_id=?',[item.s_name,item.s_price,item.s_dur,item.s_disc,item.s_ben],callback);
+    },
+    deleteService:function(s_id,callback){
+        return db.query('delete from service_tbl where s_id=?'[s_id],callback);
+    },
+    addService:function(item,callback){
+        return db.query('insert into service_tbl  (s_name,s_price,s_dur,s_disc,s_ben) values (?,?,?,?,?)',[item.s_name,item.s_price,item.s_dur,item.s_disc,item.s_ben],callback);
+    }
+} 
+    module.exports=Service;
---
+++ routes/service_routes.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var Service = require('../models/service_model');
+//var multer = require('multer');
+//var path = require('path');
+
+router.get('/', function (req, res, next) {
+    Service.getAllservice(function (err, rows) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(rows);
+        }
+    });
+});
+router.get('/:s_id', function (req, res, next) {
+    Service.getServiceById(req.params.s_id, function (err, rows) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(rows);
+        }
+    });
+});
+router.delete('/:s_id', function (req, res, next) {
+    Service.deleteService(req.params.s_id, function (err, rows) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(rows);
+        }
+    });
+});
+router.post('/', function (req, res, next) {
+    Service.addService(req.body,function (err, rows) {
+        console.log(req.body);
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(rows);
+        }
+    });
+});
+module.exports=router;
