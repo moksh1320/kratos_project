27abd7122144eb236bb357eae9183a49a131ad04
poojanmehta <poojanmehta2000@gmail.com>
2020-06-06 16:53:34 +0530

    homepage productCarousel

--- models/homepage_model.js
+++ models/homepage_model.js
@@ -1,24 +1,45 @@
 var db = require("../dbconnection");
 var homepage = {
-        getAllOffers:function(callback){
-            return db.query("select * from homeoffer_tbl",callback);
-        },
-        addOffers: function (item, filename, callback) {
-            return db.query(
-              "insert into homeoffer_tbl (offer_img,offer_link,offer_discription,) values (?,?,?,?)",
-              [
-                filename,
-                item.offer_link,
-                item.offer_discription,
-              ],
-              callback
-            );
-          },
-          deleteOffers: function (offer_id, callback) {
-            return db.query("delete from homeoffer_tbl where offer_id=?", [offer_id], callback);
-          },
-          getOfferById: function (offer_id, callback) {
-            return db.query("select * from homeoffer_tbl where offer_id=?", [offer_id], callback);
-          },
-}
-module.exports=homepage;
+  getAllOffers: function (callback) {
+    return db.query("select * from homeoffer_tbl", callback);
+  },
+  addOffers: function (item, filename, callback) {
+    return db.query(
+      "insert into homeoffer_tbl (offer_img,offer_link,offer_discription,) values (?,?,?,?)",
+      [filename, item.offer_link, item.offer_discription],
+      callback
+    );
+  },
+  deleteOffers: function (offer_id, callback) {
+    return db.query(
+      "delete from homeoffer_tbl where offer_id=?",
+      [offer_id],
+      callback
+    );
+  },
+  getOfferById: function (offer_id, callback) {
+    return db.query(
+      "select * from homeoffer_tbl where offer_id=?",
+      [offer_id],
+      callback
+    );
+  },
+  addProductsToCarousel: function (item, callback) {
+    return db.query(
+      "insert into product_carousel_tbl (fk_p_id) values (?)",
+      [item.fk_p_id],
+      callback
+    );
+  },
+  deleteProductFromCarousel: function (item, callback) {
+    return db.query(
+      "delete from product_carousel_tbl where fk_p_id=?",
+      [item.fk_p_id],
+      callback
+    );
+  },
+  getAllProducts: function (item, callback) {
+    return db.query("select * from product_carousel_tbl", callback);
+  },
+};
+module.exports = homepage;
--- routes/homepage_routes.js
+++ routes/homepage_routes.js
@@ -1,48 +1,76 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var homepageoffer = require('../models/homepage_model');
-var multer = require('multer');
-var path = require('path');
+var homepageoffer = require("../models/homepage_model");
+var multer = require("multer");
+var path = require("path");
 
 var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/images/homepageoffer_images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.filename + '_' + Date.now() + path.extname(file.originalname));
-    }
+  destination: (req, file, cb) => {
+    cb(null, "public/images/homepageoffer_images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.filename + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
 var upload = multer({ storage: storage });
 
-router.get('/', function (req, res, next) {
-    homepageoffer.getAllOffers(function (err, rows) {
-        if (err) {
-            res.json(err);
-        }
-        else {
-            res.json(rows);
-        }
-    });
+router.get("/", function (req, res, next) {
+  homepageoffer.getAllOffers(function (err, rows) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows);
+    }
+  });
 });
-router.get('/:offer_id', function (req, res, next) {
-    homepageoffer.getOffersById(req.params.offer_id, function (err, rows) {
-        if (err) {
-            res.json(err);
-        }
-        else {
-            res.json(rows);
-        }
-    });
+router.get("/:offer_id", function (req, res, next) {
+  homepageoffer.getOffersById(req.params.offer_id, function (err, rows) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows);
+    }
+  });
 });
-router.delete('/:offer_id', function (req, res, next) {
-    homepageoffer.deleteOffers(req.params.offer_id, function (err, rows) {
-        if (err) {
-            res.json(err);
-        }
-        else {
-            res.json(rows);
-        }
-    });
+router.delete("/:offer_id", function (req, res, next) {
+  homepageoffer.deleteOffers(req.params.offer_id, function (err, rows) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows);
+    }
+  });
 });
-module.exports= router;
+router.put("/", function (req, res, next) {
+  homepageoffer.deleteProductFromCarousel(req.body, function (err, rows) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+router.post("/", function (req, res, next) {
+  homepageoffer.addProductsToCarousel(req.body, function (err, rows) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+router.put("/:get", function (req, res, next) {
+  homepageoffer.getAllProducts(req.body, function (err, rows) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+module.exports = router;
